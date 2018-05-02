5dd24abc774416501b9411e90977821459bc389c
Xabier Larrakoetxea <slok69@gmail.com>
2018-05-02 12:21:57 +0200

    Implement basic structure of operator
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

--- pkg/operator/crd.go
+++ pkg/operator/crd.go
@@ -1,28 +1,45 @@
 package operator
 
 import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/tools/cache"
+
+	roleoperatorv1alpha1 "github.com/slok/role-operator/pkg/apis/roleoperator/v1alpha1"
+	"github.com/slok/role-operator/pkg/service/k8s"
 )
 
 // MultiRoleBindingCRD knows how to register and retrieve MultiRoleBinding CRs.
-type MultiRoleBindingCRD struct{}
+type MultiRoleBindingCRD struct {
+	k8ssvc k8s.Service
+}
 
-func NewMultiRoleBindingCRD() *MultiRoleBindingCRD {
-	return &MultiRoleBindingCRD{}
+// NewMultiRoleBindingCRD returns a new MultiRoleBindingCRD.
+func NewMultiRoleBindingCRD(k8ssvc k8s.Service) *MultiRoleBindingCRD {
+	return &MultiRoleBindingCRD{
+		k8ssvc: k8ssvc,
+	}
 }
 
 // Initialize satisfies crd.Interface.
 func (m *MultiRoleBindingCRD) Initialize() error {
-	return nil
+	return m.k8ssvc.MultiRoleBindingEnsureCRD()
 }
 
 // GetListerWatcher satisfies crd.Interface.
 func (m *MultiRoleBindingCRD) GetListerWatcher() cache.ListerWatcher {
-	return nil
+	return &cache.ListWatch{
+		ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+			return m.k8ssvc.MultiRoleBindingList("", options)
+		},
+		WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+			return m.k8ssvc.MultiRoleBindingWatch("", options)
+		},
+	}
 }
 
 // GetObject satisfies crd.Interface.
 func (m *MultiRoleBindingCRD) GetObject() runtime.Object {
-	return nil
+	return &roleoperatorv1alpha1.MultiRoleBinding{}
 }
--- pkg/operator/factory.go
+++ pkg/operator/factory.go
@@ -1,10 +1,11 @@
 package operator
 
 import (
-	"github.com/slok/role-operator/pkg/log"
-
 	"github.com/spotahome/kooper/operator"
 	"github.com/spotahome/kooper/operator/controller"
+
+	"github.com/slok/role-operator/pkg/log"
+	"github.com/slok/role-operator/pkg/service/k8s"
 )
 
 const (
@@ -12,11 +13,11 @@ const (
 )
 
 // New returns a new operator ready to be run.
-func New(cfg Config, logger log.Logger) operator.Operator {
+func New(cfg Config, k8ssvc k8s.Service, logger log.Logger) operator.Operator {
 	logger = logger.With("operator", operatorName)
 
 	handler := NewHandler(logger)
-	crd := NewMultiRoleBindingCRD()
+	crd := NewMultiRoleBindingCRD(k8ssvc)
 	ctrl := controller.NewSequential(cfg.ResyncDuration, handler, crd, nil, logger)
 	return operator.NewOperator(crd, ctrl, logger)
 }
